ee38f59f255fc38774f7fe43ae68e9d0cf4c4179
Tugelsikile <reyang19@gmail.com>
2022-07-03 14:33:50 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('Auth.login');
+    return view('Auth.Login');
 });
 
 Route::group(['prefix' => 'absensi'], function () {
