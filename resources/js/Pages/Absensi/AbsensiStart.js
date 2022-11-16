7433177bf46ab060166bcb37b31c444b2b47aa56
Galih Setia Adi Pradana <79183533+Mang-Agan@users.noreply.github.com>
2022-11-16 11:13:44 +0700

    commit undefined absensi start

--- resources/js/Pages/Absensi/AbsensiStart.js
+++ resources/js/Pages/Absensi/AbsensiStart.js
@@ -130,13 +130,13 @@ export default class AbsensiStart extends React.Component {
                             <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
                             <Typography variant="h6" noWrap component="a" href="/"
                                         sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>
-                                ABSENSI
+                                ABSENSIIII
                             </Typography>
                             <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
                             <Typography variant="h5" noWrap component="a"
                                         href={window.origin}
                                         sx={{ mr: 2, display: { xs: 'flex', md: 'none' }, flexGrow: 1, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }}>
-                                ABSENSI
+                                ABSENSIIII
                             </Typography>
                             <Box sx={{ flexGrow: 0 }}>
                                 <Tooltip title="Open settings">
